50cf3e73618d84e6fc305782c24396358ffd0202
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2023-06-21 15:27:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+
+    video= createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+image(video, 0, 0, 400, 400);
+}
+
+song = " ";
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function play() {
+    song.play();
+}
