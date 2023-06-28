319008fa6d2a6e19339999cef81082d26f10a4cc
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2023-06-28 15:39:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ image(video, 0, 0, 400, 400);
 
     fill('#FF0000');
     stroke('#FF0000');
-
+    
     if(scoreLeftWrist > 0.2) {
         circle(leftWristX, leftWristY, 20);
         InNumberleftWrist= Number(leftWristY);
@@ -23,7 +23,36 @@ image(video, 0, 0, 400, 400);
         document.getElementById('volume_button').innerHTML = "Volume = " + volume;
         song.setVolume(volume);
     }
-    
+
+    if(scoreRightWrist > 0.2) {
+
+    circle(rightWristX,rightWristY, 20);
+
+    if(rightWristY > 0 && rightWristY<=100) {
+        document.getElementById("speed_button").innerHTML = "Speed = " + "0.5x";
+        song.rate(0.5);
+    }
+
+    else if(rightWristY > 100 && rightWristY<= 200) {
+        document.getElementById("speed_button").innerHTML = "Speed = " + "1x";
+        song.rate(1);
+    }
+
+    else if(rightWristY > 200 && rightWristY<= 300) {
+        document.getElementById("speed_button").innerHTML = "Speed = " + "1.5x";
+        song.rate(1.5);
+    }
+
+    else if(rightWristY > 300 && rightWristY<= 400) {
+        document.getElementById("speed_button").innerHTML = "Speed = " + "2x";
+        song.rate(2);
+    }
+
+    else if(rightWristY > 400 && rightWristY<= 500) {
+        document.getElementById("speed_button").innerHTML = "Speed = " + "2.5x";
+        song.rate(2.5);
+    }
+}
 }
 
 song = " ";
@@ -33,6 +62,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload() {
     song = loadSound("music.mp3");
@@ -55,12 +85,13 @@ if(results.length > 0) {
 
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
+    scoreLeftWrist = results[0].pose.keypoints[9].score;
     console.log("leftWristX = " + leftWristX + " " + "leftWristY = " + leftWristY);
 
     rightWristX = results[0].pose.rightWrist.x;
     rightWristY = results[0].pose.rightWrist.y;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
     console.log("rightWristX = " + rightWristX + " " + "rightWristY = " + rightWristY);
 
-    scoreLeftWrist = results[0].pose.keypoints[9].score;
 }
 }
